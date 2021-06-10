fc7039dd1bc3a4fb3d468097bafe7a25f7680d49
david-stereonaut <adonbarnash@gmail.com>
2021-06-10 11:38:24 +0300

    graph progress

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { AppHeader } from './components/AppHeader/AppHeader';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { AppRouter } from './components/AppRouter';
 import { ThemeProvider } from '@material-ui/styles';
 import { theme } from './consts/theme';
